ac8e44f6f8f4928cb2fd1df5c4237ec5c32715a2
Glitch (php-contacts-abbas) <none>
2018-01-30 16:41:14 +0000

    🐬👊 Checkpoint index.html:243166/29 MVC.js:243166/267

---
+++ MVC.js
@@ -0,0 +1,4 @@
+const m = {} //MODEL (holds state variables and constants)
+const v = {} //VIEW  (holds viewable input-output components)
+const c = {} //CONTROLLER (holda all the function properties = methods)
+//=================| INITIALIZE |===============//
