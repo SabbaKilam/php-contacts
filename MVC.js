721b471d16e436c6430bcebefdefdad5546f349d
Glitch (php-contacts-abbas) <none>
2018-01-30 16:47:48 +0000

    basic template

--- MVC.js
+++ MVC.js
@@ -2,3 +2,7 @@ const m = {} //MODEL (holds state variables and constants)
 const v = {} //VIEW  (holds viewable input-output components)
 const c = {} //CONTROLLER (holda all the function properties = methods)
 //=================| INITIALIZE |===============//
+c.initialize = function(){
+
+}
+//=================| END of INITIALIZE /=======//
